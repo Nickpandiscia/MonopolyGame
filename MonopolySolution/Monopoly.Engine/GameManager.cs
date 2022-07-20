4ede464181126276f607d980c6cef33f5cea7d1d
Steve <sjwitzl@gmail.com>
2022-07-19 20:36:13 -0400

    singleton

---
+++ MonopolySolution/Monopoly.Engine/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Engine
+{
+    public class GameManager
+    {
+        private static GameManager _Instance = null;
+        public static GameManager Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new GameManager();
+                }
+                return _Instance;
+            }
+        }
+
+
+        private GameManager() 
+        {
+
+        }
+
+        public void LoadData()
+        {
+
+        }
+
+    }
+}
