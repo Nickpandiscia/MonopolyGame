922e1a6062c01f22e32b8e15425f76844e35fafe
Nickpandiscia <npandiscia1@gmail.com>
2022-07-20 18:38:03 -0400

    Put the game into a do-while loop. Did not test. Had WZ open. Would cause WZ to crash haha.

--- MonopolySolution/Monopoly/Program.cs
+++ MonopolySolution/Monopoly/Program.cs
@@ -24,45 +24,51 @@ namespace Monopoly
             //}
             //Console.ReadLine();
             var settings = new GameSettings();
-            Console.WriteLine("Do you want to configure the settings? (Type t to enter settings). If not, type f. Type exit to quit.");
-            var settingAnswer = Console.ReadLine();
-            if(settingAnswer == "exit")
+            Console.WriteLine("Hit any button to continue, or type exit to quit the game.");
+            var exitAnswer = Console.ReadLine();
+            do
             {
-                return;
-            }
-            if (settingAnswer == "t")
-            {
-                Console.WriteLine($"How many players are there?");
-                var playerCount = int.Parse(Console.ReadLine());
+                Console.WriteLine("Do you want to configure the settings? (Type t to enter settings). If not, type f. Type exit to quit.");
+                var settingsAnswer = Console.ReadLine();
+                if(exitAnswer == "exit")
+                {
+                    break;
+                }
+                if (settingsAnswer == "t")
+                {
+                    Console.WriteLine($"How many players are there?");
+                    var playerCount = int.Parse(Console.ReadLine());
 
-                Console.WriteLine($"How much currency do you want to start with?");
-                var currencyCount = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"How much currency do you want to start with?");
+                    var currencyCount = int.Parse(Console.ReadLine());
 
-                settings.PlayerCount = playerCount;
-                settings.StartingPlayerBalance = currencyCount;
-                settings.StartingProperties = 0;
-            }
-            else
-            {
-               // Logger.Log("Player chose not to configure");
-            }
-           // GameManager.Instance.SetGameSettings(settings);
-
-
-            using (SpeechSynthesizer synth = new SpeechSynthesizer())
-            {
-
-                // Configure the audio output.   
-                synth.SetOutputToDefaultAudioDevice();
-                // Speak the contents of the prompt synchronously.  
-                synth.Speak("Wake up. The game settings have been set bitch.");
-
-                synth.Speak("Welcome to Monopoly! Lets begin.");
-
-                
+                    settings.PlayerCount = playerCount;
+                    settings.StartingPlayerBalance = currencyCount;
+                    settings.StartingProperties = 0;
+                }
+                else
+                {
+                    // Logger.Log("Player chose not to configure");
+                }
+                // GameManager.Instance.SetGameSettings(settings);
 
 
-            }
+                using (SpeechSynthesizer synth = new SpeechSynthesizer())
+                {
+
+                    // Configure the audio output.   
+                    synth.SetOutputToDefaultAudioDevice();
+                    // Speak the contents of the prompt synchronously.  
+                    synth.Speak("Wake up. The game settings have been set bitch.");
+
+                    synth.Speak("Welcome to Monopoly! Lets begin.");
+
+
+
+
+                }
+            }while (exitAnswer != "exit");
+            
 
 
 
